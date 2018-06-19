64696be4498fee2a44c1eb95d86d6069ec34a781
Konstantin Bukin <bukin@inetstudio.ru>
2018-06-20 02:56:15 +0300

    Remove defer bindings

--- src/Providers/DummiesServiceProvider.php
+++ src/Providers/DummiesServiceProvider.php
@@ -23,6 +23,16 @@ class DummiesServiceProvider extends ServiceProvider
     }
 
     /**
+     * Регистрация привязки в контейнере.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->registerBindings();
+    }
+
+    /**
      * Регистрация команд.
      *
      * @return void
@@ -80,5 +90,59 @@ class DummiesServiceProvider extends ServiceProvider
     protected function registerViews(): void
     {
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'admin.module.dummies');
+    }
+
+    /**
+     * Регистрация привязок, алиасов и сторонних провайдеров сервисов.
+     *
+     * @return void
+     */
+    protected function registerBindings(): void
+    {
+        // Controllers
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Controllers\Back\DummiesControllerContract', 'InetStudio\Dummies\Http\Controllers\Back\DummiesController');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Controllers\Back\DummiesDataControllerContract', 'InetStudio\Dummies\Http\Controllers\Back\DummiesDataController');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Controllers\Back\DummiesUtilityControllerContract', 'InetStudio\Dummies\Http\Controllers\Back\DummiesUtilityController');
+        
+        // Events
+        $this->app->bind('InetStudio\Dummies\Contracts\Events\Back\ModifyDummyEventContract', 'InetStudio\Dummies\Events\Back\ModifyDummyEvent');
+
+        // Mails
+        $this->app->bind('InetStudio\Dummies\Contracts\Mail\NewDummyMailContract', 'InetStudio\Dummies\Mail\NewDummyMail');
+
+        // Models
+        $this->app->bind('InetStudio\Dummies\Contracts\Models\DummyModelContract', 'InetStudio\Dummies\Models\DummyModel');
+
+        // Notifications
+        $this->app->bind('InetStudio\Dummies\Contracts\Notifications\NewDummyNotificationContract', 'InetStudio\Dummies\Notifications\NewDummyNotification');
+        $this->app->bind('InetStudio\Dummies\Contracts\Notifications\NewDummyQueueableNotificationContract', 'InetStudio\Dummies\Notifications\NewDummyQueueableNotification');
+
+        // Observers
+        $this->app->bind('InetStudio\Dummies\Contracts\Observers\DummyObserverContract', 'InetStudio\Dummies\Observers\DummyObserver');
+        
+        // Repositories
+        $this->app->bind('InetStudio\Dummies\Contracts\Repositories\DummiesRepositoryContract', 'InetStudio\Dummies\Repositories\DummiesRepository');
+        
+        // Requests
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Requests\Back\SaveDummyRequestContract', 'InetStudio\Dummies\Http\Requests\Back\SaveDummyRequest');
+        
+        // Responses
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Dummies\DestroyResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Dummies\DestroyResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Dummies\FormResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Dummies\FormResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Dummies\IndexResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Dummies\IndexResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Dummies\SaveResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Dummies\SaveResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Dummies\ShowResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Dummies\ShowResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Utility\SlugResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Utility\SlugResponse');
+        $this->app->bind('InetStudio\Dummies\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract', 'InetStudio\Dummies\Http\Responses\Back\Utility\SuggestionsResponse');
+        
+        // Services
+        $this->app->bind('InetStudio\Dummies\Contracts\Services\Back\DummiesDataTableServiceContract', 'InetStudio\Dummies\Services\Back\DummiesDataTableService');
+        $this->app->bind('InetStudio\Dummies\Contracts\Services\Back\DummiesObserverServiceContract', 'InetStudio\Dummies\Services\Back\DummiesObserverService');
+        $this->app->bind('InetStudio\Dummies\Contracts\Services\Back\DummiesServiceContract', 'InetStudio\Dummies\Services\Back\DummiesService');
+        $this->app->bind('InetStudio\Dummies\Contracts\Services\Front\DummiesServiceContract', 'InetStudio\Dummies\Services\Front\DummiesService');
+        
+        // Transformers
+        $this->app->bind('InetStudio\Dummies\Contracts\Transformers\Back\DummyTransformerContract', 'InetStudio\Dummies\Transformers\Back\DummyTransformer');
+        $this->app->bind('InetStudio\Dummies\Contracts\Transformers\Back\SuggestionTransformerContract', 'InetStudio\Dummies\Transformers\Back\SuggestionTransformer');
     }
 }
